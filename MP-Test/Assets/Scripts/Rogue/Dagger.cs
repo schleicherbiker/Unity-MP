cfe3daa00bcca99f232ce483c2992a60dc1c6d60
Alec Schleicher <alecs@live.unc.edu>
2017-08-11 16:37:28 -0400

    Started moveset for rogue character

---
+++ MP-Test/Assets/Scripts/Rogue/Dagger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dagger : MonoBehaviour {
+
+	[SerializeField] private float DAGGER_SPEED;
+	private Rigidbody2D myRB; 
+	private BoxCollider2D myCollider;
+
+	void Start ()
+	{
+		myRB = GetComponent<Rigidbody2D>();
+		myCollider = GetComponent<BoxCollider2D>();
+
+		myRB.velocity = new Vector2(DAGGER_SPEED, 0);
+	}
+
+}
---
+++ MP-Test/Assets/Scripts/Rogue/Rogue_Abilities.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Rogue_Abilities : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// Return If Not Local Player
+		if (!isLocalPlayer)
+            return;
+
+		HandleInput();
+	}
+
+	void HandleInput()
+	{
+		// First Move
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			Debug.Log("First move!");
+		}
+
+		// Second Move
+		if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			Dash();
+			Debug.Log("Second move!");
+		}
+
+		// Third Move
+		if (Input.GetKeyDown(KeyCode.Alpha3)) 
+		{
+			ThrowDagger();
+			Debug.Log("Third move!");
+		}
+
+		// Ultimate Move
+		if (Input.GetKeyDown(KeyCode.Alpha4)) {
+			Debug.Log("Ultimate move!");
+		}
+	}
+
+	void Dash()
+	{
+
+	}
+
+
+	void ThrowDagger()
+	{
+
+	}
+}
