b839a04abeca2ea8fd902a2adc6e485e7fa7b865
Alec Schleicher <alecs@live.unc.edu>
2017-09-02 18:41:39 -0400

    Added functional healthbars

--- MP-Test/Assets/Scripts/Health.cs
+++ MP-Test/Assets/Scripts/Health.cs
@@ -1,20 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
 
-public class Health : MonoBehaviour {
+public class Health : NetworkBehaviour {
 
 	public const int maxHealth = 150;
-	public int currentHealth = maxHealth;
+	[SyncVar(hook="OnChangeHealth")] public int currentHealth = maxHealth;
+	public int numLives = 3;
+	public RectTransform healthBar;
+	private float healthBarSize;
+
+	public void Start()
+	{
+		healthBarSize = healthBar.sizeDelta.x;
+	}
 
 	public void TakeDamage(int amount)
 	{
+		// Check if server...
+		if (!isServer)
+			return;
+
+		// Take Damage
 		currentHealth -= amount;
+
+		// Check health/lives
 		if (currentHealth <= 0)
 		{
+			gameObject.transform.position = GameObject.Find("SpawnPoint" + gameObject.GetComponent<SetTeam>().team).transform.position;
 			currentHealth = 0;
-			Debug.Log("Dead!");
+			numLives--;
+			if (numLives == 0)
+			{
+				Debug.Log("Somebody wins!");
+			}
 		}
 	}
 	
+	void OnChangeHealth(int currentHealth)
+	{
+		healthBar.sizeDelta = new Vector2(((float)currentHealth/(float)maxHealth)*healthBarSize, healthBar.sizeDelta.y);
+	}
 }
--- MP-Test/Assets/Scripts/SpawnPlayers.cs
+++ MP-Test/Assets/Scripts/SpawnPlayers.cs
@@ -3,16 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnPlayers : MonoBehaviour {
-
 	[SerializeField] private GameObject spawn1;
 	[SerializeField] private GameObject spawn2;
-
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
